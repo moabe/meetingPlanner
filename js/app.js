7c7ad19bf4fae4c09223a2f051fa46ff69a1ef68
moabe <moa.bergsmark@gmail.com>
2014-03-13 14:53:57 +0100

    A dayView with everything for a day
    Also some comments added

--- js/app.js
+++ js/app.js
@@ -19,12 +19,12 @@ $(function() {
 	var activityListView = new ActivityListView($("#activityListView"), model);
 	var singleActivityView = new SingleActivityView($("#singleActivityView"), model, 0, 0);
 
-	var dayHeaderView = new DayHeaderView($("#dayScheduleView"), model, 0);
+	var dayView = new DayView($("#dayView"), model, 0);
 
 	var singelActivityViewController = new SingleActivityViewController(singleActivityView, model);
 
-	var dayScheduleView = new DayScheduleView($("#daySchedule"), model, 0);
+	//var dayScheduleView = new DayScheduleView($("#dayScheduleView"), model, 0);
 
-	var dayScheduleViewController = new DayScheduleViewController(dayScheduleView, model);
+	var dayScheduleViewController = new DayScheduleViewController(dayView, model);
 
 });
---
+++ js/views/dayView.js
@@ -0,0 +1,60 @@
+//DayHeaderView Object constructor
+var DayView = function(container, model, dayId) {
+
+	this.endTime = container.find("#endTime");
+	this.totalTime = container.find("#totalTime");
+	this.dayScheduleView = container.find("#dayScheduleView");
+
+
+
+	this.dayId = dayId;
+
+	model.addObserver(this);
+
+
+
+	this.updateView = function() {
+
+
+		/*
+		Fills the list of dragable activities in the day
+		<ul id="dayScheduleView" class="list-unstyled"></ul>
+		*/
+		this.dayScheduleView.empty();
+
+		for (var i = 0; i < model.days[dayId]._activities.length; ++i) {
+			var a = $("<li>");
+			a.addClass("panel panel-info");
+			a.html(model.days[dayId]._activities[i].getName());
+			this.dayScheduleView.append(a);
+		}
+
+
+		/*
+	Puts end time and total time into the header of a day. 
+	Gets the data from the model. 
+	<div class="row">
+		<div class= "col-md-8" >
+			<div><p id="endTime">End time: </p></div>
+			<p id="totalTime"> Total time: </p>
+		</div>
+	</div>*/
+
+
+		this.endTime.append(model.days[dayId].getEnd());
+		this.totalTime.append(model.days[dayId].getTotalLength() + " min");
+	}
+
+
+
+	this.updateView();
+
+	this.update = function(args) {
+		this.updateView();
+	}
+
+
+	//This function gets called when there is a change at the model
+
+
+}
