cc9771e73016abb89af0b0e4cd62993b89c507f2
moabe <moa.bergsmark@gmail.com>
2014-03-14 11:46:44 +0100

    some progress on chart

--- js/views/dayView.js
+++ js/views/dayView.js
@@ -3,23 +3,44 @@ var DayView = function(container, model, dayId) {
 
 	this.endTime = container.find("#endTime");
 	this.totalTime = container.find("#totalTime");
+	this.chart = container.find("#chart");
 	this.dayScheduleView = container.find("#dayScheduleView");
 	//need to get this one in the controller
 	this.inputStartTime = container.find("#inputStartTime");
-	this.chart = container.find("#chart");
-
 
 
 
 	this.dayId = dayId;
 
+
+
 	model.addObserver(this);
 
-		var chartDiv = $("<div>");
-		chartDiv.addClass("chartDiv");
-		var activityLength = model.days[dayId].getLengthByType(1);
-		console.log(activityLength);
-		var procentage = activityLength/model.days[dayId].getTotalLength();
+
+
+
+	var chartDiv = $("<div>");
+	chartDiv.attr("id","chartDiv");
+
+	var percentage = (model.days[dayId].getLengthByType(1)/model.days[dayId].getTotalLength())*100 + "%";
+	console.log(percentage);
+
+	
+
+	chartDiv.css("width", percentage);
+
+
+
+
+	chartDiv.addClass("presentationColor");
+
+
+
+
+	this.chart.append(chartDiv);
+
+
+
 
 
 
@@ -60,6 +81,8 @@ var DayView = function(container, model, dayId) {
 		//  getLengthByType() I need to get length of all types and then change styling from this
 		//Divide the lengthbyType with total length to set a width parameter
 		
+
+
 
 		
 
