89fa5c3c8fc85a670c2d622e22a09484a1524429
tereseno <terese_n@hotmail.com>
2014-03-19 11:56:36 +0100

    fixing floating bug

--- js/controllers/parkedActivitiesViewController.js
+++ js/controllers/parkedActivitiesViewController.js
@@ -1,8 +1,15 @@
 // ParkedActivitiesViewController constructor
 var ParkedActivitiesViewController = function (view, model) {
 	
+	$(window).resize(function () {
+		if (view.floatingView.hasClass("affix")) {
+			view.floatingView.css("left", view.floatingView.parent().offset().left);
+			view.floatingView.css("width", view.floatingView.parent().width());
+		}
+	});
+	
 	view.floatingView.on("affix.bs.affix", function() { // Before becoming floating
-		view.floatingView.css("left", view.floatingView.offset().left);
+		view.floatingView.css("left", view.floatingView.parent().offset().left + view.floatingView.position().left);
 		view.floatingView.css("width", view.floatingView.parent().width());
 	});
 	
