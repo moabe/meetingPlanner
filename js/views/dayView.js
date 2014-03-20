9b5a7d525a6a2343de879e53cd86c1b98a339e64
moabe <moa.bergsmark@gmail.com>
2014-03-20 12:04:14 +0100

    comments

--- js/views/dayView.js
+++ js/views/dayView.js
@@ -22,6 +22,7 @@ var DayView = function(container, model, dayId) {
 	this.breakP = this.container.find("#breakP");
 
 
+//breakdiv is the div which the class making the line marking 30% breaks will be appended
 	var breakdiv = $("<div>");
 	this.chart.append(breakdiv);
 
@@ -29,9 +30,11 @@ var DayView = function(container, model, dayId) {
 
 	model.addObserver(this);
 
+
+	//In the uptadet view function all parts of the day view that will be updated is placed. 
 	this.updateView = function() {
 		
-		// If first time filling the view
+		// When day is created it creates a activitylist view where activities can be dropped
 		if (this.dayScheduleView == null) {
 			this.dayScheduleView = this.container.find("#dayScheduleView");
 			
@@ -40,11 +43,13 @@ var DayView = function(container, model, dayId) {
 			window.dragAndDropLists.push("#"+this.dayScheduleView.attr("id"));
 			window.linkDragAndDropLists();
 		}
-		
+		//When the view updates the endtime and total time is emptied
+		//The breack paragraph also gets emtied 
 		this.endTime.empty();
 		this.totalTime.empty();
 		this.breakP.empty();
 
+		//Setting day title, start time, end time and total time from model
 		this.dayName.html("Day "+ (+dayId + 1));
 		this.inputStartTime.attr("value", "0" + model.days[dayId].getStart() + "0");
 		this.endTime.append("End time: "+ model.days[dayId].getEnd());
@@ -54,21 +59,31 @@ var DayView = function(container, model, dayId) {
 		//Divide the lengthbyType with total length to set a width parameter
 
 
+
+
+
+
+		//Sets the width of the diffrent div for showing the total amount of presentaion,
+		//group work, discussion and break activities
+		//it's donr by dividing the total amount of every type of activity and then dividing with the 
+		//total time of the day
+		//they are not showed from begining becouse width = 0%
 		this.pPart.css("width", (model.days[dayId].getLengthByType(0)/model.days[dayId].getTotalLength())*100 + "%");
 		this.gwPart.css("width", (model.days[dayId].getLengthByType(1)/model.days[dayId].getTotalLength())*100 + "%");
 		this.dPart.css("width", (model.days[dayId].getLengthByType(2)/model.days[dayId].getTotalLength())*100 + "%");
 		this.bPart.css("width", (model.days[dayId].getLengthByType(3)/model.days[dayId].getTotalLength())*100 + "%");
 
 
-		/* make a line div for showing how much is 30% breaks 
-		<div class="minBreak"></div>*/
-
-		if(model.days[dayId].getLengthByType(0) != 0 || model.days[dayId].getLengthByType(1) != 0 || model.days[dayId].getLengthByType(2) != 0|| model.days[dayId].getLengthByType(3) != 0){
+		/* The styling class minBreak is added to the breakdiv for showing the total time isn't 0*/
+		if( model.days[dayId].getTotalLength()!=0){
 			breakdiv.addClass("minBreak");
 
+			//If breaks are less then 30 % a text shows up, this is also dependent on that there actually is 
+			// a total time
 			if((model.days[dayId].getLengthByType(3)/model.days[dayId].getTotalLength())*100 < 30){
 				this.breakP.html("Not enough breaks!").attr("style","color:red");
 			}
+			//Else if there is 30% or more there will be a text showing that there is 30% or more breaks
 			else{
 				this.breakP.html("You're day have 30% or more breaks, great!").attr("style","color:black");
 			}
